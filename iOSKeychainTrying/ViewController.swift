96470ab44227789b7474308113a36881f98b4a9c
Muammer Akca <muammerakca10@gmail.com>
2022-10-06 19:25:34 +0300

    notificationCenter moved to viewDidLoad

--- iOSKeychainTrying/ViewController.swift
+++ iOSKeychainTrying/ViewController.swift
@@ -8,25 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var authenticateTapped: UIButton!
-
+    
     @IBOutlet var secret: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func authenticateTapped(_ sender: Any) {
+        
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
-    @objc func adjustForKeyboard {
+    @IBAction func authenticateTapped(_ sender: Any) {
         
     }
-
+    
+    
+    
+    @objc func adjustForKeyboard (notification : Notification) {
+        
+    }
+    
 }
 
